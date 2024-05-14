396d652aaf1c2801dc9dce4372afe11f2a0d653c
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-05-14 15:30:54 -0400

    Part 5 full circle complete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Part 1
 const string =
-  "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
 function parseCSV(string) {
   let strArr = string.split("\n");
@@ -43,3 +43,65 @@ function parseObj(array) {
 }
 
 console.log(parseObj(parseCSVCol(string)));
+
+//Part 4
+let arrObj = parseObj(parseCSVCol(string));
+
+//1. Remove the last element from the sorted array
+arrObj.pop();
+//2. insert object at index1
+let barryObj = { id: "48", name: "Barry", occupation: "Runner", age: "25" };
+arrObj.splice(1, 0, barryObj);
+//3.add to the end of the array
+arrObj.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
+
+console.log(arrObj);
+
+//4. find average age of the group
+function calcAvg(array) {
+  let sum = 0;
+
+  console.log(array.length);
+
+  for (let i = 0; i < array.length; i++) {
+    sum += parseInt(array[i].age);
+  }
+
+  return sum / array.length;
+}
+
+console.log(calcAvg(arrObj));
+
+//Helper function flattens the multidimensional arrays
+//flatten
+function flatten(array) {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        newArr[newArr.length] = array[i][j];
+      }
+    } else {
+      newArr[newArr.length] = array[i];
+    }
+  }
+
+  return newArr;
+}
+
+//Converts our array of objects into a csv
+function parseArrObj(array) {
+  let csvArray = [];
+
+  csvArray.push(Object.keys(array[0]));
+  for (let i = 0; i < array.length; i++) {
+    csvArray.push(Object.values(array[i]));
+  }
+
+  let flattenedArray = flatten(csvArray);
+  let joinedString = flattenedArray.join(",");
+
+  return joinedString;
+}
+
+console.log(parseArrObj(arrObj));
